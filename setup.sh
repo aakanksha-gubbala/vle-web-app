f1be20a368058f0cdb9d46e3e9245d9a02df1d86
Aakanksha Gubbala <46394857+kraanky@users.noreply.github.com>
2021-04-14 11:15:33 +0530

    ~

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ email = \"aakanksha.gubbala@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]
+primaryColor = '#0013F7'
+backgroundColor = '#FFFFFF'
+secondaryBackgroundColor = '#12E279'
+textColor = '#000000'
+font = 'monospace'
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
